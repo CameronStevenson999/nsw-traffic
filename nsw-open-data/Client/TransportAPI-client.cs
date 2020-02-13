3e020b4e1ba0ce7c718ed527b25d3d0f8f002d05
cordate <51943426+CameronStevenson999@users.noreply.github.com>
2020-02-13 16:50:17 +1000

    Print api results to console

---
+++ nsw-open-data/Client/TransportAPI-client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using nsw_open_data.Models;
+
+namespace nsw_open_data.Client
+{
+    public class TransportAPI_client
+    {
+        //static HttpClient client = new HttpClient();
+        public async Task<string> GetCurrentTrafficIncidentsAsync(Uri path, HttpClient transportApiClient)
+        {
+            string incidents = null;
+
+            HttpResponseMessage response = await transportApiClient.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                incidents = await response.Content.ReadAsStringAsync();
+            }
+            return incidents;
+        }
+    }
+}
--- nsw-open-data/Program.cs
+++ nsw-open-data/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using nsw_open_data.Client;
 
 namespace nsw_open_data
 {
     class Program
     {
-        static void Main(string[] args)
+        static HttpClient client = new HttpClient();
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting async process to call NSW Open Data Transport API");
+            RunAsync().GetAwaiter().GetResult();
+        }
+
+        static async Task RunAsync()
+        {
+            // Update port # in the following line.
+            client.BaseAddress = new Uri("https://api.transport.nsw.gov.au/v1/live/hazards/incident/open");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/octet-stream"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("apikey", "j1zHbAwDsYNo4sR9FpInRVzIX8698p9JxomI");
+
+            TransportAPI_client transportClient = new TransportAPI_client();
+            var url = client.BaseAddress.ToString();
+
+            try
+            {
+                // Get the current traffic incidents
+                var incidents = await transportClient.GetCurrentTrafficIncidentsAsync(client.BaseAddress, client);
+
+                Console.WriteLine(incidents.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }
